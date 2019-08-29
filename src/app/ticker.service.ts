c7e2d7a18a7a9d99ec49c9e80bb4dba5ed197640
Peyton Baker-Johnson <p.bakerjohnson@gmail.com>
2019-08-29 14:11:14 -0500

    Added a lot of bizarre type declarations to the ticker service.

--- src/app/ticker.service.ts
+++ src/app/ticker.service.ts
@@ -9,7 +9,9 @@ export class TickerService {
      public tickObservable: Observable<{}>;
 
      // WORTHLESS - This gets passed around a whole lot but doesn't actually accomplish anything. Just like yer mum. OHHHHHH
-     private numberOfDays: number = 1;
+     private numberOfDays = 1;
+
+     private intervalAsMilliseconds = 1000;
 
      // Putting the list of subscribed observers in the service's scope instead
      // of inside a function to be passed around in that scope.
@@ -20,14 +22,14 @@ export class TickerService {
      }
 
      // GAME LOOP - This creates the game clock as an observable things can subscribe to. I probably could have just had
-     private tickSubscriber = function() {
+     private tickSubscriber: () => (observer: any) => { unsubscribe(): void } = function() {
           // Capture the scope to refer to it reliably; it needs to be captured because "this" isn't a reliable way
           // to refer to it, given that this is a function that returns a function that calls a function with a
           // parameter that has its own scope (or at least, it did during testing)
           const self = this;
 
           // Return the subscriber function? Runs when subscribe() is invoked
-          return function(observer: any) {
+          return function(observer: any): { unsubscribe(): void } {
                // Collects the observer
                self.subscribedObservers.push(observer);
                // If this is the first subscription, this starts the wait() loop that runs the clock.
@@ -36,11 +38,11 @@ export class TickerService {
                     // Starts the function that
                     self.initializeLoopingTick(
                          {
-                              next(numberOfDays: number) {
+                              next(numberOfDays: number): void {
                                    // Iterate through all observers and emit for them.
                                    self.subscribedObservers.forEach((obs: Observer<number>) => obs.next(numberOfDays));
                               },
-                              complete() {
+                              complete(): void {
                                    // Notify all complete callbacks to
                                    this.subscribedObservers.slice(0).forEach((obs: Observer<number>) => obs.complete());
                               }
@@ -57,6 +59,6 @@ export class TickerService {
      private initializeLoopingTick(observer: { next: any; complete?: () => void; }): void {
           window.setInterval(() => {
                observer.next(this.numberOfDays);
-          }, 1000);
+          }, this.intervalAsMilliseconds);
      }
 }

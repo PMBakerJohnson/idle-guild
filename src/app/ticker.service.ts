86ff8b6dc889b5acffceac935a18dacc80ae8cef
Peyton Baker-Johnson <p.bakerjohnson@gmail.com>
2019-08-29 11:56:35 -0500

    Refactored ticker to comply with field ordering coding standards.

--- src/app/ticker.service.ts
+++ src/app/ticker.service.ts
@@ -6,20 +6,24 @@ import { Observable, Observer } from 'rxjs';
 })
 export class TickerService {
 
-     constructor() { }
+     public tickObservable: Observable<{}>;
 
      // WORTHLESS - This gets passed around a whole lot but doesn't actually accomplish anything. Just like yer mum. OHHHHHH
      private numberOfDays: number = 1;
 
      // Putting the list of subscribed observers in the service's scope instead
-          // of inside a function to be passed around in that scope.
+     // of inside a function to be passed around in that scope.
      private subscribedObservers: Observer<number>[] = new Array();
+
+     constructor() {
+          this.tickObservable = new Observable(this.tickSubscriber());
+     }
 
      // GAME LOOP - This creates the game clock as an observable things can subscribe to. I probably could have just had
      private tickSubscriber = function() {
           // Capture the scope to refer to it reliably; it needs to be captured because "this" isn't a reliable way
-               // to refer to it, given that this is a function that returns a function that calls a function with a
-               // parameter that has its own scope (or at least, it did during testing)
+          // to refer to it, given that this is a function that returns a function that calls a function with a
+          // parameter that has its own scope (or at least, it did during testing)
           const self = this;
 
           // Return the subscriber function? Runs when subscribe() is invoked
@@ -27,20 +31,20 @@ export class TickerService {
                // Collects the observer
                self.subscribedObservers.push(observer);
                // If this is the first subscription, this starts the wait() loop that runs the clock.
-                    // I'm not sure this is a good way to have the idle functionality exist? It works, for now.
+               // I'm not sure this is a good way to have the idle functionality exist? It works, for now.
                if (self.subscribedObservers.length === 1) {
                     // Starts the function that
                     self.initializeLoopingTick(
-                    {
-                         next(numberOfDays: number) {
-                              // Iterate through all observers and emit for them.
-                              self.subscribedObservers.forEach( (obs: Observer<number>) => obs.next(numberOfDays));
-                         },
-                         complete() {
-                              // Notify all complete callbacks to
-                              this.subscribedObservers.slice(0).forEach((obs: Observer<number>) => obs.complete());
-                         }
-                    });
+                         {
+                              next(numberOfDays: number) {
+                                   // Iterate through all observers and emit for them.
+                                   self.subscribedObservers.forEach((obs: Observer<number>) => obs.next(numberOfDays));
+                              },
+                              complete() {
+                                   // Notify all complete callbacks to
+                                   this.subscribedObservers.slice(0).forEach((obs: Observer<number>) => obs.complete());
+                              }
+                         });
                }
                return {
                     unsubscribe() {
@@ -50,10 +54,9 @@ export class TickerService {
                };
           };
      };
-     private initializeLoopingTick(observer: { next: any; complete?: () => void; } ): void {
+     private initializeLoopingTick(observer: { next: any; complete?: () => void; }): void {
           window.setInterval(() => {
                observer.next(this.numberOfDays);
           }, 1000);
      }
-     public tickObservable = new Observable(this.tickSubscriber());
 }

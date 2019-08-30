d94210af4716be4c66689c60b8cca14d033a8056
Peyton Baker-Johnson <p.bakerjohnson@gmail.com>
2019-08-30 10:31:05 -0500

    Fix for the helper functions not being arrow functions.

--- src/app/helper-functions.ts
+++ src/app/helper-functions.ts
@@ -1,12 +1,12 @@
-export function getRandomInteger(max: number, min: number): number {
+export const getRandomInteger = (max: number, min: number): number => {
      let randomInteger: number;
 
      randomInteger = Math.floor(Math.random() * (max - min + 1) + min);
 
      return randomInteger;
-}
+};
 
-export function fromJSON(object: any, objectAsJson: any): any {
+export const fromJSON = (object: any, objectAsJson: any): any => {
      for (const propName in objectAsJson) {
           if (object.hasOwnProperty(propName)) {
                object[propName] = objectAsJson[propName];
@@ -14,4 +14,4 @@ export function fromJSON(object: any, objectAsJson: any): any {
      }
 
      return object;
-}
+};
